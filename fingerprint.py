d82bb143ee4166d4d2399eeae9fdf3298ed9567c
Evan Chang <evanc577@gmail.com>
2019-04-06 13:56:33 -0500

    formatting changes

--- fingerprint.py
+++ fingerprint.py
@@ -22,7 +22,9 @@ THRESHOLD_RATE = 0.5        # min score per second required
 THRESHOLD_RATE_MIN_TIME = 3 # min time for THRESHOLD_RATE to become active (sec)
 THRESHOLD_RATE_MIN = 12     # min score required at THRESHOLD_RATE_MIN_TIME
 TIMEOUT = 15                # max duration to listen for (sec)
-TOGGLE_BITS = 14            # number of bits to fuzz when searching fingerprint matches
+TOGGLE_BITS = 16            # number of bits to fuzz when searching fingerprint
+                            # matches. Values: 0 - 22. (may increase robustness
+                            # but increase false positive rate and system load)
 
 # global variables
 id_error = False
@@ -62,7 +64,8 @@ def stft(data, win_len=100, win_overlap=10):
         else:
             frame = win * data[frame_start : frame_start + win_len_frames]
         fft_frame = abs(fft(frame))
-        stft_out[i,:] = np.square(np.abs(fft_frame[:win_len_frames//2])) / win_len_frames
+        stft_out[i,:] = np.square(np.abs(fft_frame[:win_len_frames//2])) \
+                / win_len_frames
 
     return stft_out
 
@@ -89,7 +92,8 @@ def compute_mel(data, nbins=18):
     f_bank = np.where(f_bank < 0, 0, f_bank)
 
     filter_banks = np.dot(data, f_bank.T)
-    filter_banks = np.where(filter_banks <= 0, np.finfo(float).eps, filter_banks)
+    filter_banks = \
+            np.where(filter_banks <= 0, np.finfo(float).eps, filter_banks)
     return 20 * np.log10(filter_banks)
 
 # find peaks, compute fingerprints, insert or query database
@@ -137,32 +141,60 @@ def find_peaks(data, query=True, ID=None):
                 r_2d = (data[t-1][b-2] + data[t][b-2] + data[t+1][b-2]) / 3
 
             # region 3
-            r_3a = (data[t-2][b+1] + data[t-1][b+1] + data[t][b+1] + data[t-2][b] + data[t-1][b]) / 5
-            r_3b = (data[t][b+1] + data[t+1][b+1] + data[t+2][b+1] + data[t+1][b] + data[t+2][b]) / 5
-            r_3c = (data[t+1][b] + data[t+2][b] + data[t][b-1] + data[t+1][b-1] + data[t+2][b-1]) / 5
-            r_3d = (data[t-2][b] + data[t-1][b] + data[t-2][b-1] + data[t-1][b-1] + data[t][b-1]) / 5
+            r_3a = (data[t-2][b+1] + data[t-1][b+1] \
+                    + data[t][b+1] + data[t-2][b] + data[t-1][b]) / 5
+            r_3b = (data[t][b+1] + data[t+1][b+1] \
+                    + data[t+2][b+1] + data[t+1][b] + data[t+2][b]) / 5
+            r_3c = (data[t+1][b] + data[t+2][b] \
+                    + data[t][b-1] + data[t+1][b-1] + data[t+2][b-1]) / 5
+            r_3d = (data[t-2][b] + data[t-1][b] \
+                    + data[t-2][b-1] + data[t-1][b-1] + data[t][b-1]) / 5
 
             # region 4
             if b == data.shape[1] - 2:
-                r_4a = (data[t-9][b+1] + data[t-8][b+1] + data[t-7][b+1] + data[t-6][b+1]) / 4
-                r_4b = (data[t-5][b+1] + data[t-4][b+1] + data[t-3][b+1] + data[t-2][b+1]) / 4
-                r_4e = (data[t+5][b+1] + data[t+4][b+1] + data[t+3][b+1] + data[t+2][b+1]) / 4
-                r_4f = (data[t+9][b+1] + data[t+8][b+1] + data[t+7][b+1] + data[t+6][b+1]) / 4
+                r_4a = (data[t-9][b+1] + data[t-8][b+1] \
+                        + data[t-7][b+1] + data[t-6][b+1]) / 4
+                r_4b = (data[t-5][b+1] + data[t-4][b+1] \
+                        + data[t-3][b+1] + data[t-2][b+1]) / 4
+                r_4e = (data[t+5][b+1] + data[t+4][b+1] \
+                        + data[t+3][b+1] + data[t+2][b+1]) / 4
+                r_4f = (data[t+9][b+1] + data[t+8][b+1] \
+                        + data[t+7][b+1] + data[t+6][b+1]) / 4
             else:
-                r_4a = (data[t-9][b+2] + data[t-8][b+2] + data[t-7][b+2] + data[t-6][b+2] + data[t-9][b+1] + data[t-8][b+1] + data[t-7][b+1] + data[t-6][b+1]) / 8
-                r_4b = (data[t-5][b+2] + data[t-4][b+2] + data[t-3][b+2] + data[t-2][b+2] + data[t-5][b+1] + data[t-4][b+1] + data[t-3][b+1] + data[t-2][b+1]) / 8
-                r_4e = (data[t+5][b+2] + data[t+4][b+2] + data[t+3][b+2] + data[t+2][b+2] + data[t+5][b+1] + data[t+4][b+1] + data[t+3][b+1] + data[t+2][b+1]) / 8
-                r_4f = (data[t+9][b+2] + data[t+8][b+2] + data[t+7][b+2] + data[t+6][b+2] + data[t+9][b+1] + data[t+8][b+1] + data[t+7][b+1] + data[t+6][b+1]) / 8
+                r_4a = (data[t-9][b+2] + data[t-8][b+2] + data[t-7][b+2] 
+                        + data[t-6][b+2] + data[t-9][b+1] + data[t-8][b+1] \
+                        + data[t-7][b+1] + data[t-6][b+1]) / 8
+                r_4b = (data[t-5][b+2] + data[t-4][b+2] + data[t-3][b+2] \
+                        + data[t-2][b+2] + data[t-5][b+1] + data[t-4][b+1] \
+                        + data[t-3][b+1] + data[t-2][b+1]) / 8
+                r_4e = (data[t+5][b+2] + data[t+4][b+2] + data[t+3][b+2] \
+                        + data[t+2][b+2] + data[t+5][b+1] + data[t+4][b+1] \
+                        + data[t+3][b+1] + data[t+2][b+1]) / 8
+                r_4f = (data[t+9][b+2] + data[t+8][b+2] + data[t+7][b+2] \
+                        + data[t+6][b+2] + data[t+9][b+1] + data[t+8][b+1] \
+                        + data[t+7][b+1] + data[t+6][b+1]) / 8
             if b == 1:
-                r_4c = (data[t-9][b-1] + data[t-8][b-1] + data[t-7][b-1] + data[t-6][b-1]) / 4
-                r_4d = (data[t-5][b-1] + data[t-4][b-1] + data[t-3][b-1] + data[t-2][b-1]) / 4
-                r_4g = (data[t+5][b-1] + data[t+4][b-1] + data[t+3][b-1] + data[t+2][b-1]) / 4
-                r_4h = (data[t+9][b-1] + data[t+8][b-1] + data[t+7][b-1] + data[t+6][b-1]) / 4
+                r_4c = (data[t-9][b-1] + data[t-8][b-1] \
+                        + data[t-7][b-1] + data[t-6][b-1]) / 4
+                r_4d = (data[t-5][b-1] + data[t-4][b-1] \
+                        + data[t-3][b-1] + data[t-2][b-1]) / 4
+                r_4g = (data[t+5][b-1] + data[t+4][b-1] \
+                        + data[t+3][b-1] + data[t+2][b-1]) / 4
+                r_4h = (data[t+9][b-1] + data[t+8][b-1] \
+                        + data[t+7][b-1] + data[t+6][b-1]) / 4
             else:
-                r_4c = (data[t-9][b-2] + data[t-8][b-2] + data[t-7][b-2] + data[t-6][b-2] + data[t-9][b-1] + data[t-8][b-1] + data[t-7][b-1] + data[t-6][b-1]) / 8
-                r_4d = (data[t-5][b-2] + data[t-4][b-2] + data[t-3][b-2] + data[t-2][b-2] + data[t-5][b-1] + data[t-4][b-1] + data[t-3][b-1] + data[t-2][b-1]) / 8
-                r_4g = (data[t+5][b-2] + data[t+4][b-2] + data[t-3][b-2] + data[t+2][b-2] + data[t+5][b-1] + data[t+4][b-1] + data[t+3][b-1] + data[t+2][b-1]) / 8
-                r_4h = (data[t+9][b-2] + data[t+8][b-2] + data[t+7][b-2] + data[t+6][b-2] + data[t+9][b-1] + data[t+8][b-1] + data[t+7][b-1] + data[t+6][b-1]) / 8
+                r_4c = (data[t-9][b-2] + data[t-8][b-2] + data[t-7][b-2] \
+                        + data[t-6][b-2] + data[t-9][b-1] + data[t-8][b-1] \
+                        + data[t-7][b-1] + data[t-6][b-1]) / 8
+                r_4d = (data[t-5][b-2] + data[t-4][b-2] + data[t-3][b-2] \
+                        + data[t-2][b-2] + data[t-5][b-1] + data[t-4][b-1] \
+                        + data[t-3][b-1] + data[t-2][b-1]) / 8
+                r_4g = (data[t+5][b-2] + data[t+4][b-2] + data[t-3][b-2] \
+                        + data[t+2][b-2] + data[t+5][b-1] + data[t+4][b-1] \
+                        + data[t+3][b-1] + data[t+2][b-1]) / 8
+                r_4h = (data[t+9][b-2] + data[t+8][b-2] + data[t+7][b-2] \
+                        + data[t+6][b-2] + data[t+9][b-1] + data[t+8][b-1] \
+                        + data[t+7][b-1] + data[t+6][b-1]) / 8
 
 
             # compute mask
@@ -417,12 +449,15 @@ def audio_callback(indata, frames, callback_time, status):
             hist[dt] += 1
 
             # check if score is above defined threshold
-            if hist[dt] >= THRESHOLD:
+            if (elapsed_sec < THRESHOLD_RATE_MIN_TIME and \
+                    hist[dt] >= THRESHOLD_RATE_MIN) \
+                    or hist[dt] >= THRESHOLD:
                 temp_found = True
             elif elapsed_sec >= THRESHOLD_RATE_MIN_TIME \
-                    and hist[dt] >= (elapsed_sec - THRESHOLD_RATE_MIN_TIME) * \
-                        THRESHOLD_RATE + THRESHOLD_RATE_MIN:
+                    and hist[dt] >= (elapsed_sec - THRESHOLD_RATE_MIN_TIME) \
+                    * THRESHOLD_RATE + THRESHOLD_RATE_MIN:
                 temp_found = True
+
             if temp_found and hist[dt] > id_score:
                 id_score = hist[dt]
                 id_hash = m
@@ -453,7 +488,6 @@ def get_song_info(song_id):
 
 # return a stream from the microphone
 def record_mic():
-    print('Listening', end='')
     stream = sounddevice.InputStream(samplerate=FS, channels=1,
             blocksize=REC_FRAME_SIZE, callback=audio_callback)
     return stream
@@ -483,6 +517,7 @@ def test():
     with stream:
         start = time.time()
         stream.start()
+        print('Listening', end='')
         try:
             song_id, score, elapsed_bin = identify_song()
         except LookupError:
